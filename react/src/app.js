8a96fdc5ccd25a3c0609d2aa1fe4f1c345d61d69
Claudio Cruz <claudio.cruz.1996@gmail.com>
2021-02-25 16:13:28 +0000

    nodejs added to react-app project

--- react/src/app.js
+++ react/src/app.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+    componentDidMount() {
+        axios.get('http://localhost:3000/users')
+         .then(response => {
+           console.log(response.data);
+         })
+    }
     render() {
         return (
-            <h1>Welcome teste to !</h1>
+            <h1>Welcome to react!</h1>
         )
     }
 }
