2af0e3cbb723cffe802ec4a4d31c4c8d981219f5
Claudio Cruz <claudio.cruz.1996@gmail.com>
2021-05-07 17:46:08 +0100

    data-model

--- Node-CronJob/backup-db-script.sql
+++ Node-CronJob/backup-db-script.sql
@@ -6,10 +6,11 @@ CREATE SEQUENCE hipertensao_id_seq AS integer START WITH 1 INCREMENT BY 1 NO MIN
 CREATE TABLE public.hipertensao
 (
     id integer NOT NULL DEFAULT nextval('hipertensao_id_seq'::regclass),
-	contagem_de_utentes_inscritos_com_hipertensao_arterial_com_pres integer NOT NULL,
+	utentes_hipertensao_pa_menor_150_90_mmhg_n integer NOT NULL,
     regiao character varying(250) COLLATE pg_catalog."default" NOT NULL,
-	ponto_ou_localizacao_geografica character varying(250) COLLATE pg_catalog."default" NOT NULL,
-    proporcao_hipertensos_65_a_com_pa_150_90 real NOT NULL,
+	lat double precision NOT NULL,
+    long double precision NOT NULL,
+    hipertensos_65_anos_pa_150_90 real NOT NULL,
     tempo date NOT NULL,
     aces character varying(250) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT hipertensao_pkey PRIMARY KEY (id)
@@ -20,13 +21,14 @@ CREATE SEQUENCE diabetes_id_seq AS integer START WITH 1 INCREMENT BY 1 NO MINVAL
 CREATE TABLE public.diabetes
 (
     id integer NOT NULL DEFAULT nextval('diabetes_id_seq'::regclass),
-    contagem_de_utentes_inscritos_com_diabetes_com_ultimo_resultado_de_hgba1c_inferior_ou_igual_a_8_0 integer NOT NULL,
+    utentes_com_hgba1c_inferior_ou_igual_8_0 integer NOT NULL,
     regiao character varying(250) COLLATE pg_catalog."default" NOT NULL,
+    lat double precision NOT NULL,
+    long double precision NOT NULL,
     tempo date NOT NULL,
-	ponto_ou_localizacao_geografica character varying(250) COLLATE pg_catalog."default" NOT NULL,
-    proporcao_dm_c_ultima_hgba1c_8_0 integer NOT NULL,
-	contagem_de_utentes_inscritos_com_diabetes_com_exame_dos_pes_realizado_no_ultimo_ano integer NOT NULL,
-	proporcao_dm_com_exame_pes_ultimo_ano integer NOT NULL,
+    proporcao_dm_ultima_hgba1c_8_0 real NOT NULL,
+	utentes_com_exame_dos_pes_realizado_no_ultimo_ano integer NOT NULL,
+	proporcao_dm_com_exame_pes_ultimo_ano real NOT NULL,
     aces character varying(250) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT diabetes_pkey PRIMARY KEY (id)
 )
