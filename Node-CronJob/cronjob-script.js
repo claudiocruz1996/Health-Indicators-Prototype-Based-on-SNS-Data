96f8e51a1389ce1e6eebdd0fa563ce79bc90aa6d
Claudio Cruz <claudio.cruz.1996@gmail.com>
2021-05-13 15:59:15 +0100

    node-cron-new-data-models

--- Node-CronJob/cronjob-script.js
+++ Node-CronJob/cronjob-script.js
@@ -20,32 +20,64 @@ let myNewObject = []
 
 /**
  * This function creates a new object from the recived data, then it deletes all rows present on the dataset table, and inserts the new freshly created object data.
- * @param {*} data This variable stores a JSON with all data about the dataset called
- * @param {*} dataModel This variable loads the Data Model correspond to the dataset called
- * @param {*} dataset This variable passes the Dataset name
- * @param {*} client This variable contains the database pool connection
+ * @param {String} tableName This variable stores the table name
+ * @param {JSON Object} data This variable stores a JSON with all data about the dataset called
+ * @param {Object} dataModel This variable loads the Data Model correspond to the dataset called
+ * @param {String} dataset This variable passes the Dataset name
+ * @param {Pool} client This variable contains the database pool connection
  */
-async function executeQuery(data, dataModel, dataset, client) {
+async function executeQuery(tableName, data, dataModel, dataset, client) {
   try {
+    const dropTable = await client.query(`DELETE FROM ${tableName} WHERE true;`)
     data.records.forEach((element) => {
       let data = {}
       Object.keys(element.fields).forEach((field) => {
-        if (field == "ponto_ou_localizacao_geografica") {
-          data["lat"] = element.fields[field][0]
-          data["long"] = element.fields[field][1]
-        } else if (field == "tempo") {
-          data["tempo"] = element.fields[field] + "-01"
-        } else {
-          data[mapping[dataset][field]] = element.fields[field]
+        if (field != "id") {
+          if (field == "ponto_ou_localizacao_geografica" || field == "localizacao_geografica") {
+            data["lat"] = element.fields[field][0]
+            data["long"] = element.fields[field][1]
+          } else if (field == "tempo" || field == "periodo") {
+            data["tempo"] = element.fields[field] + "-01"
+          } else {
+            data[mapping[tableName][field]] = element.fields[field]
+          }
         }
       })
       myNewObject.push(data)
     })
 
-    let datasetInsert = dataModel.insert(myNewObject).toQuery()
-    const dropTable = await client.query(`DELETE FROM ${dataset} WHERE true;`)
-    const resDatasetInsert = await client.query(datasetInsert)
-    console.log(`Os dados para o indicador ${dataset} foram atualizados`)
+    //console.log(myNewObject)
+
+    let i = 0
+    while (i < myNewObject.length - 1) {
+      let dataInsert = myNewObject.slice(i, i + 3000 < myNewObject.length ? i + 3000 : myNewObject.length)
+      let dataQuery = dataModel.insert(dataInsert).toQuery()
+      await pool.query(dataQuery)
+      i += 3000
+    }
+
+    /*let i = 0
+    let myObjectTemp = []
+    myNewObject.forEach((element) => {
+      if (i >= 3000) {
+        let datasetInsert = dataModel.insert(myObjectTemp).toQuery()
+        const resDatasetInsert = pool.query(datasetInsert)
+        myObjectTemp = []
+        i = 0
+      }
+      myObjectTemp.push(element)
+      i++
+    })
+
+    if (myObjectTemp.length > 0) {
+      let datasetInsert = dataModel.insert(myObjectTemp).toQuery()
+      const resDatasetInsert = await pool.query(datasetInsert)
+    }
+
+ 
+    //let datasetInsert = dataModel.insert(myNewObject).toQuery()
+    //const resDatasetInsert = await client.query(datasetInsert)*/
+    console.log(`Status:[Atualizado]  Os dados para o indicador ${dataset} foram atualizados`)
     myNewObject = []
   } catch (err) {
     console.log(err.stack)
@@ -54,24 +86,22 @@ async function executeQuery(data, dataModel, dataset, client) {
 /**
  * This function verifies if the dataset table is updated, if is not, it calls the transparency API to get all present data available about the dataset
  * and then calls the function executeQuery().
- * @param {*} dataset This variable passes the Dataset name
- * @param {*} nhits This variable stores the number of rows with data about the dataset called
- * @param {*} dataModel This variable loads the Data Model correspond to the dataset called
- * @param {*} client This variable contains the database pool connection
+ * @param {String} tableName This variable stores the table name
+ * @param {String} dataset This variable passes the Dataset name
+ * @param {Integer} nhits This variable stores the number of rows with data about the dataset called
+ * @param {Object} dataModel This variable loads the Data Model correspond to the dataset called
+ * @param {Pool} client This variable contains the database pool connection
  */
-async function axiosCallDataset(dataset, nhits, dataModel, client) {
+async function axiosCallDataset(tableName, dataset, nhits, dataModel, client) {
   try {
-    let resTableRow = await client.query(`SELECT COUNT (*) FROM ${dataset};`)
+    let resTableRow = await client.query(`SELECT COUNT (*) FROM ${tableName};`)
     if (resTableRow.rows[0].count != nhits) {
       const resp = await axios.get(
         `https://transparencia.sns.gov.pt/api/records/1.0/search/?dataset=${dataset}&q=&rows=${nhits}`
       )
-
-      return await executeQuery(resp.data, dataModel, dataset, client)
+      return await executeQuery(tableName, resp.data, dataModel, dataset, client)
     } else {
-      console.log(
-        `Os dados para o indicador ${dataset} já se encontram atualizados`
-      )
+      console.log(`Status:[Normal]  Os dados para o indicador ${dataset} já se encontram atualizados`)
     }
   } catch (err) {
     console.log(err.stack)
@@ -80,17 +110,18 @@ async function axiosCallDataset(dataset, nhits, dataModel, client) {
 
 /**
  * This function calls the transparency API to get the number of rows in the dataset.
- * @param {string} dataset This variable passes the Dataset name
+ * @param {String} tableName This variable stores the table name
+ * @param {String} dataset This variable passes the Dataset name
  * @param {Object} dataModel This variable loads the Data Model correspond to the dataset called
  */
-async function axiosCallNhits(dataset, dataModel) {
+async function axiosCallNhits(tableName, dataset, dataModel) {
   const client = await pool.connect()
   try {
     const resp = await axios.get(
       `https://transparencia.sns.gov.pt/api/records/1.0/search/?dataset=${dataset}&q=&rows=0`
     )
 
-    return await axiosCallDataset(dataset, resp.data.nhits, dataModel, client)
+    return await axiosCallDataset(tableName, dataset, resp.data.nhits, dataModel, client)
   } catch (err) {
     console.log(err)
   } finally {
@@ -101,10 +132,22 @@ async function axiosCallNhits(dataset, dataModel) {
 /**
  * This Scheduler runs every first day of every month at 01:00h. [0 1 1 * *]
  */
-cron.schedule("*/30 * * * * *", async function () {
+cron.schedule("*/10 * * * * *", async function () {
   try {
-    await axiosCallNhits("hipertensao", dataModels.hipertensao)
-    await axiosCallNhits("diabetes", dataModels.diabetes)
+    /*  await axiosCallNhits("hipertensao", "hipertensao", dataModels.hipertensao)
+    await axiosCallNhits("diabetes", "diabetes", dataModels.diabetes)
+    await axiosCallNhits("saude_da_mulher_e_crianca", "saude-da-mulher-e-crianca", dataModels.saude_da_mulher_e_crianca)
+    await axiosCallNhits("rastreios_oncologicos", "rastreios-oncologicos", dataModels.rastreios_oncologicos)
+    await axiosCallNhits(
+      "registo_de_testamentos_vitais",
+      "registo-de-testamentos-vitais",
+      dataModels.registo_de_testamentos_vitais
+    ) */
+    await axiosCallNhits(
+      "referenciacoes_soep_emitidas_nos_centros_de_saude",
+      "referenciacoes-soep-emitidas-nos-centros-de-saude",
+      dataModels.referenciacoes_soep_emitidas_nos_centros_de_saude
+    )
   } catch (err) {
     console.log(err)
   }
--- Node-CronJob/dataModels.js
+++ Node-CronJob/dataModels.js
@@ -17,7 +17,43 @@ const diabetes = sql.define({
   columns: Object.values(mapping["diabetes"]),
 })
 
+/**
+ * This variable define the health of women and children Data Model used to convert JSON data into a insert query statement
+ */
+const saude_da_mulher_e_crianca = sql.define({
+  name: "saude_da_mulher_e_crianca",
+  columns: Object.values(mapping["saude_da_mulher_e_crianca"]),
+})
+
+/**
+ * This variable define the cancer screenings Data Model used to convert JSON data into a insert query statement
+ */
+const rastreios_oncologicos = sql.define({
+  name: "rastreios_oncologicos",
+  columns: Object.values(mapping["rastreios_oncologicos"]),
+})
+
+/**
+ * This variable define the registration of vital wills Data Model used to convert JSON data into a insert query statement
+ */
+const registo_de_testamentos_vitais = sql.define({
+  name: "registo_de_testamentos_vitais",
+  columns: Object.values(mapping["registo_de_testamentos_vitais"]),
+})
+
+/**
+ * This variable define soep references issued in health centers Data Model used to convert JSON data into a insert query statement
+ */
+const referenciacoes_soep_emitidas_nos_centros_de_saude = sql.define({
+  name: "referenciacoes_soep_emitidas_nos_centros_de_saude",
+  columns: Object.values(mapping["referenciacoes_soep_emitidas_nos_centros_de_saude"]),
+})
+
 module.exports = {
   hipertensao,
   diabetes,
+  saude_da_mulher_e_crianca,
+  rastreios_oncologicos,
+  registo_de_testamentos_vitais,
+  referenciacoes_soep_emitidas_nos_centros_de_saude,
 }
